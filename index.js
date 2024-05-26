834eb9a7a0bef4da94aa58f191413258b024f142
Gihan Chamila <algihanchamila@gmail.com>
2024-05-26 19:42:30 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express")
+const { default: mongoose } = require("mongoose")
 
 const PORT = 8000
 
 // init app
 const app = express()
+
+// Connect database
+const connectionUrl = "mongodb://localhost:27017/todoDb"
+
+mongoose.connect(connectionUrl).then(() => {
+    console.log("Database connected successfully")
+}).catch((error) => {
+    console.log(error.message)
+});
 
 // view engine
 app.set("view engine", "ejs")
